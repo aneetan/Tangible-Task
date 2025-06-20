ca5e6e39c221a10942a81aec2d6bcae5e55d66ae
aneetan <anitaneupane2061@gmail.com>
2025-06-20 11:23:30 +0545

    Routing configured

---
+++ Form-And-Controlled-Inputs/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import './App.css'
+import Index from './pages/Index'
+
+function App() {
+
+  return (
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index/>}/> 
+        </Routes>
+       </BrowserRouter>
+    </>
+  )
+}
+
+export default App
